9b65b6a2f53bb3ccf643458613442bcbf2f1393d
Amina <minassouane@gmail.com>
2025-07-25 04:02:48 +0100

    Implement tabbed browsing

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import "./contact.js";
 console.log("Its running.");
 
 let buttons = document.querySelectorAll("button");
+let sections = document.querySelectorAll("section");
 
 function tabbedBrowsing() {
   for (let button of buttons) {
-    button.addEventListener("click", () => {});
+    button.addEventListener("click", () => {
+      for (let section of sections) {
+        section.style.display = "none";
+      }
+
+      let section = document.querySelector(`section.${button.className}`);
+      section.style.display = "block";
+    });
   }
 }
+
+tabbedBrowsing();
