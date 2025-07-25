88508fb00d8519327367ffe0c8180a2af7e26aae
Amina <minassouane@gmail.com>
2025-07-25 03:51:02 +0100

    Add food pictures and lights

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import "./menu.js";
 import "./contact.js";
 
 console.log("Its running.");
+
+let buttons = document.querySelectorAll("button");
+
+function tabbedBrowsing() {
+  for (let button of buttons) {
+    button.addEventListener("click", () => {});
+  }
+}
